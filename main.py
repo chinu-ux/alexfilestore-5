a05e8f1c418259c0f27e5d6ee4379fb8e00da3d3
chinu-ux <bilashinikhatua@gmail.com>
2025-10-31 17:28:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
 
 if __name__ == "__main__":
     Bot().run()
+
+import time
+
+print("✅ Bot is running... Waiting for messages...")
+
+while True:
+    time.sleep(10)
